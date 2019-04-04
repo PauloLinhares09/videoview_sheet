3dd2420c094d8f39ebd2846c2e421e0dfc08daba
Paulo Linhares <paulolinhares@PAULOLINHARES.local>
2019-04-04 16:12:01 -0300

    layout improv video

--- app/src/main/java/com/packapps/videoview/MainActivity.kt
+++ app/src/main/java/com/packapps/videoview/MainActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,10 +18,12 @@ class MainActivity : AppCompatActivity() {
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
+                testOpenBlankFragment()
 
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
+                testOpenBlankFragment()
 
                 return@OnNavigationItemSelectedListener true
             }
@@ -28,11 +31,18 @@ class MainActivity : AppCompatActivity() {
         false
     }
 
+    private fun testOpenBlankFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, BlankFragment.newInstance())
+        transaction.commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
     }
 
 }
