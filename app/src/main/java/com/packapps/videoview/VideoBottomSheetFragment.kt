2d64376d2d59c95a93da3d6a771746de6018e7c4
Paulo Linhares <paulolinhares@PAULOLINHARES.local>
2019-04-04 10:19:24 -0300

    with dialog fragment expanded

---
+++ app/src/main/java/com/packapps/videoview/VideoBottomSheetFragment.kt
@@ -0,0 +1,43 @@
+package com.packapps.videoview
+
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class VideoBottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.activity_scrolling, container, false)
+    }
+
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        var d = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+
+        d?.setOnShowListener(object : DialogInterface.OnShowListener{
+            override fun onShow(dialog: DialogInterface?) {
+
+                val bottomSheet = d.findViewById(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout?
+
+                BottomSheetBehavior.from(bottomSheet).state = BottomSheetBehavior.STATE_EXPANDED
+            }
+        })
+
+        return d
+    }
+
+
+}
