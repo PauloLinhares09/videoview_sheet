958cf378ac21ef038513ac3011c61bd0c21d1f26
Paulo Linhares <paulolinhares@PAULOLINHARES.local>
2019-04-04 14:00:05 -0300

    callback bottom sheet is ok

--- app/src/main/java/com/packapps/videoview/ViewListFragment.kt
+++ app/src/main/java/com/packapps/videoview/ViewListFragment.kt
@@ -1,15 +1,13 @@
 package com.packapps.videoview
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.content_view_list_fragment.view.*
-import kotlinx.android.synthetic.main.fragment_view_list.*
 import kotlinx.android.synthetic.main.fragment_view_list.view.*
 
 
@@ -28,6 +26,17 @@ class ViewListFragment : Fragment() {
             bottomSheetBehavior.isHideable = false
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
             bottomSheetBehavior.peekHeight = 600
+
+            bottomSheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback(){
+                override fun onSlide(view: View, positionFloat: Float) {
+                    Log.i("TAG", "positionFloat: " + positionFloat)
+                }
+
+                override fun onStateChanged(view: View, positionState: Int) {
+                    Log.i("TAG", "state: " + positionState)
+                }
+
+            })
         }
 
         return mView
