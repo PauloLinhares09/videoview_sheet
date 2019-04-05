5014d85e2893511ba732cf22c92af6b15208f26e
Paulo Linhares <paulolinhares@PAULOLINHARES.local>
2019-04-05 12:50:19 -0300

    text showing

--- app/src/main/java/com/packapps/videoview/ViewListFragment.kt
+++ app/src/main/java/com/packapps/videoview/ViewListFragment.kt
@@ -34,7 +34,7 @@ class ViewListFragment : Fragment() {
             bottomSheetBehavior = BottomSheetBehavior.from(mView.bottomSheetVideo)
             bottomSheetBehavior.isHideable = false
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-            bottomSheetBehavior.peekHeight = 450
+            bottomSheetBehavior.peekHeight = 500
 
             bottomSheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback(){
                 override fun onSlide(view: View, positionFloat: Float) {
