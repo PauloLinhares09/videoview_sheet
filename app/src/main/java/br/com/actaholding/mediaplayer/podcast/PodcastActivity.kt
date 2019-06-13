d048d5bb94eac991f9ecb2bdabd0ab0dcb1b6d20
Paulo Linhares <paulolinhares@PAULOLINHARES.local>
2019-06-13 15:15:37 -0300

    offset

--- app/src/main/java/br/com/actaholding/mediaplayer/podcast/PodcastActivity.kt
+++ app/src/main/java/br/com/actaholding/mediaplayer/podcast/PodcastActivity.kt
@@ -1,24 +1,67 @@
 package br.com.actaholding.mediaplayer.podcast
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import br.com.actaholding.mediaplayer.R
+import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.activity_podcast.*
 
-class PodcastActivity : AppCompatActivity() {
+class PodcastActivity : AppCompatActivity(), AppBarLayout.OnOffsetChangedListener {
+
+    var mCurrentState = State.EXPANDED
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podcast)
         setSupportActionBar(toolbar)
 
+
         collapsingToolbar.setExpandedTitleColor(ActivityCompat.getColor(this, android.R.color.transparent))
         collapsingToolbar.setCollapsedTitleTextColor(ActivityCompat.getColor(this, android.R.color.transparent))
 
+        app_bar.addOnOffsetChangedListener(this)
 
 
 
+
+    }
+
+
+
+    override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
+        appBarLayout?.let {
+
+            if (verticalOffset == 0) {
+                if (mCurrentState != State.EXPANDED) {
+                    toolbar.elevation = 5F
+                    containerControlls.elevation = 0F
+                }
+                mCurrentState = State.EXPANDED
+            } else if (Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()) {
+                if (mCurrentState != State.COLLAPSED) {
+                    toolbar.elevation = 0F
+                    containerControlls.elevation = 10F
+                }
+                mCurrentState = State.COLLAPSED
+            } else {
+                if (mCurrentState != State.IDLE) {
+                    toolbar.elevation = 5F
+                    containerControlls.elevation = 0F
+                }
+                mCurrentState = State.IDLE
+            }
+        }
+    }
+
+
+
+    enum class State{
+        EXPANDED,
+        COLLAPSED,
+        IDLE
     }
 }
