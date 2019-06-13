fed626b6fc2e49242f56b23078bb61d05caa9f81
Paulo Linhares <paulolinhares@PAULOLINHARES.local>
2019-06-13 10:58:53 -0300

    posdcast

--- app/src/main/java/br/com/actaholding/mediaplayer/podcast/PodcastActivity.kt
+++ app/src/main/java/br/com/actaholding/mediaplayer/podcast/PodcastActivity.kt
@@ -3,6 +3,7 @@ package br.com.actaholding.mediaplayer.podcast
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import br.com.actaholding.mediaplayer.R
 import kotlinx.android.synthetic.main.activity_podcast.*
 
@@ -16,5 +17,12 @@ class PodcastActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        collapsingToolbar.setExpandedTitleColor(ActivityCompat.getColor(this, android.R.color.transparent))
+        collapsingToolbar.setCollapsedTitleTextColor(ActivityCompat.getColor(this, android.R.color.transparent))
+
+
+
+
     }
 }
