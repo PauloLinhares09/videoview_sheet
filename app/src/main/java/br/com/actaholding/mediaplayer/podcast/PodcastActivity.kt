1488b037428c58f7793abe79de5ab8b786dcf7dc
Paulo Linhares <paulolinhares@PAULOLINHARES.local>
2019-06-14 11:42:37 -0300

    shader in text is work

--- app/src/main/java/br/com/actaholding/mediaplayer/podcast/PodcastActivity.kt
+++ app/src/main/java/br/com/actaholding/mediaplayer/podcast/PodcastActivity.kt
@@ -1,13 +1,17 @@
 package br.com.actaholding.mediaplayer.podcast
 
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import br.com.actaholding.mediaplayer.R
 import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.activity_podcast.*
+import kotlinx.android.synthetic.main.content_podcast.*
 
 class PodcastActivity : AppCompatActivity(), AppBarLayout.OnOffsetChangedListener {
 
@@ -19,6 +23,7 @@ class PodcastActivity : AppCompatActivity(), AppBarLayout.OnOffsetChangedListene
         setContentView(R.layout.activity_podcast)
         setSupportActionBar(toolbar)
 
+        applyShaderGradient(399F)
 
         collapsingToolbar.setExpandedTitleColor(ActivityCompat.getColor(this, android.R.color.transparent))
         collapsingToolbar.setCollapsedTitleTextColor(ActivityCompat.getColor(this, android.R.color.white))
@@ -29,17 +34,21 @@ class PodcastActivity : AppCompatActivity(), AppBarLayout.OnOffsetChangedListene
 
 
 
+
     override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
         appBarLayout?.let {
+            Log.i("TAG", "verticalOffset:L ${verticalOffset}")
 
             if (verticalOffset == 0) {
                 if (mCurrentState != State.EXPANDED) {
                     tvSubTitleCollapsed.visibility = View.GONE
+                    applyShaderGradient(399F)
                 }
                 mCurrentState = State.EXPANDED
             } else if (Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()) {
                 if (mCurrentState != State.COLLAPSED) {
                     tvSubTitleCollapsed.visibility = View.VISIBLE
+                    applyShaderGradient(0F)
 
                 }
                 mCurrentState = State.COLLAPSED
@@ -47,10 +56,26 @@ class PodcastActivity : AppCompatActivity(), AppBarLayout.OnOffsetChangedListene
                 if (mCurrentState != State.IDLE) {
 //                    toolbar.elevation = 5F
 //                    containerControlls.elevation = 0F
+                    applyShaderGradient(0F)
                 }
                 mCurrentState = State.IDLE
             }
         }
+    }
+
+
+    private fun applyShaderGradient(y1: Float) {
+        val shader: Shader = LinearGradient(
+            0F,
+            0F,
+            0F,
+            y1,
+            intArrayOf(Color.WHITE, Color.TRANSPARENT),
+            floatArrayOf(0F, 1F),
+            Shader.TileMode.CLAMP
+        )
+        tvContentDescription.paint.setShader(shader)
+        tvContentDescription.invalidate()
     }
 
 
